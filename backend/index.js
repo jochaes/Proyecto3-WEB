0dfcc9a0a76d31cc89595d863bfd56b6fefd2deb
Josué Chaves <d4n11083@estudiantec.cr>
2023-11-14 20:19:44 -0600

    comentarios

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,36 @@
+//Librerias necesarias para crear el servidor
 const express = require("express")
 const bodyParser = require("body-parser")
 const admin = require("firebase-admin")
 const cors = require("cors")
 const morgan = require("morgan")
 
-const app = express()
-const port = 3000 // Choose any port you prefer
 
-// Firebase configuration - replace with your Firebase credentials
+// Para crear un servidor con Express, debemos crear una instancia de la clase express().
+const app = express()
+
+// Configuracin con Firebase
 const serviceAccount = require("./firebase/formularios-6d195-firebase-adminsdk-601o8-fb0146a834.json")
 
+// Inicializamos admin con la configuracin de Firebase
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 })
 
+// Inicializamos Firestore
 const firestore = admin.firestore();
 
+// Configuracin de CORS
 const allowedOrigins = ["http://192.168.100.2:3006", "http://localhost:3006", "https://formularios-6d195.web.app", "https://formularios-6d195.firebaseapp.com"] // Add the allowed origins
 
+// Configuramos CORS con las opciones necesarias
 const corsOptions = {
 	origin: allowedOrigins,
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	optionsSuccessStatus: 200
 }
+
+// Middleware para imprimir en consola las peticiones que llegan al servidor
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -31,15 +39,25 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+//Middleware para imprimir en consola las peticiones que llegan al servidor
 app.use(requestLogger)
+
 app.use(morgan("dev"))
+
+// Middleware para configurar CORS
 app.use(cors(corsOptions))
+
+// Middleware para configurar el body parser
 app.use(bodyParser.json())
 
-// app.get("/", (req, res) => {
-// 	res.send("Hello World!")
-// })
 
+// Ruta inicial
+app.get("/", (req, res) => {
+	res.send("Hello World!")
+})
+
+
+// Ruta para obtener todos los formularios
 app.get("/api/getForms", async (req, res) => {
 	try {
 		const formsRef = firestore.collection("formularios")
@@ -57,12 +75,8 @@ app.get("/api/getForms", async (req, res) => {
 	}
 })
 
-
-
+// Ruta para obtener un formulario por su ID
 app.post("/api/uploadForm", async (req, res) => {
-	// console.log("*******")
-	// console.log(req.body)
-	// console.log("*******")
 
 	const { nombre_formulario, campos_formulario } = req.body
 
@@ -87,6 +101,8 @@ app.post("/api/uploadForm", async (req, res) => {
 })
 
 
+
+//Iniciamos la aplicacion en el puerto 3001
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
