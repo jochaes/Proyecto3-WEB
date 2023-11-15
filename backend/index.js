1e20d561116977c03f80d45d6fbbdb5074ca1c1e
Josué Chaves <d4n11083@estudiantec.cr>
2023-11-15 15:10:42 -0600

    Se pueden guardar formularios

--- backend/index.js
+++ backend/index.js
@@ -75,10 +75,27 @@ app.get("/api/getForms", async (req, res) => {
 	}
 })
 
+//Ruta para Obtener un formulario por su ID en el body
+app.post("/api/getForm", async (req, res) => {
+	const { id } = req.body
+
+	try {
+		const formsRef = firestore.collection("formularios")
+		const snapshot = await formsRef.doc(id).get()
+		const form = snapshot.data()
+
+		res.status(200).json(form)
+	} catch (error) {
+		console.error('Error getting form:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+
 // Ruta para obtener un formulario por su ID
 app.post("/api/uploadForm", async (req, res) => {
 
-	const { nombre_formulario, campos_formulario } = req.body
+	const { nombre_formulario, campos_formulario} = req.body
 
 	try {
 		const formsRef = firestore.collection("formularios")
@@ -91,14 +108,60 @@ app.post("/api/uploadForm", async (req, res) => {
 
     console.log(`Form uploaded with ID: ${result.id}`);
     res.status(200).json({ message: 'Form uploaded successfully' });
-
-
 	} catch (error) {
     console.error('Error uploading form:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 
 })
+
+//Rutarpara guardar la respuesta de un formulario con la referencia del formulario
+app.post("/api/uploadAnswer", async (req, res) => {
+
+	const { id_formulario, campos_formulario} = req.body
+
+	try {
+		const formsRef = firestore.collection("respuestas")
+
+		const result = await formsRef.add({
+			id_formulario: id_formulario,
+			campos_formulario: campos_formulario,
+			fecha_creacion: new Date().toISOString()
+		});
+
+		console.log(`Answer uploaded with ID: ${result.id}`);
+		res.status(200).json({ message: 'Answer uploaded successfully' });
+	} catch (error) {
+		console.error('Error uploading answer:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+
+})
+
+//Ruta para obtener todas las respuestas de un formulario por su ID
+app.post("/api/getAnswers", async (req, res) => {
+	const { id } = req.body
+
+	try {
+		const formsRef = firestore.collection("respuestas")
+		const snapshot = await formsRef.where("id_formulario", "==", id).get()
+		const answers = []
+
+		snapshot.forEach((doc) => {
+			answers.push({ id: doc.id, ...doc.data() })
+		})
+
+		res.status(200).json(answers)
+	} catch (error) {
+		console.error('Error getting answers:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+
+
+
+
 
 
 
